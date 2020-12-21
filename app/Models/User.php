793ec3372acb7e8f14f27b32568dff0a5772609a
unknown <juhyeon1114@gmail.com>
2020-12-21 16:40:33 +0900

    Liking and unliking posts 까지 들음

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Post;
+use App\Models\Like;
 
 class User extends Authenticatable
 {
@@ -45,5 +47,10 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
