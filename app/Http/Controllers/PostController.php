7a26f5cce239bbd1d4d71e1b5945e6b49e609b7e
unknown <juhyeon1114@gmail.com>
2020-12-22 10:54:48 +0900

    수강 완료

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only(['store', 'destroy']);
+    }
+
     public function index()
     {
         // $posts = Post::orderBy('created_at', 'desc')->with(['user', 'likes'])->paginate(10);
